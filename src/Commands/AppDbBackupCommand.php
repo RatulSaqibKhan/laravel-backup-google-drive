31aca252bda1547ff65c716431718699143d808a
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2022-05-09 16:58:54 +0600

    Initial Commit with Backup command

---
+++ src/Commands/AppDbBackupCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ratulsaqibkhan\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+
+class AppDbBackupCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laravel-db:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Taking Backup of Database and Storage Folder';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        if (config('backup.backup_enable')) {
+            File::delete(File::glob(base_path('backup/*.zip')));
+            File::delete(File::glob(storage_path('app/public/*.zip')));
+            Artisan::call('backup:run');
+            if (config('backup.enable_copy_to_local_dir')) {
+                File::copyDirectory('backup', config('backup.local_backup_dir'));
+            }
+        }
+
+    }
+}
---
+++ src/LaravelBackupGoogleDriveServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ratulsaqibkhan\LaravelBackupGoogleDrive;
+
+use Illuminate\Support\ServiceProvider;
+use Ratulsaqibkhan\Console\Commands\AppDbBackupCommand;
+
+class LaravelBackupGoogleDriveServiceProvider extends ServiceProvider
+{
+    protected $rootPath;
+
+    /**
+    * Register any application services.
+    *
+    * @return void
+    */
+    public function register()
+    {
+        $this->rootPath = realpath(__DIR__.'/../');
+    }
+
+    public function boot()
+    {
+        $this->loadCommands();
+    }
+
+    private function loadCommands()
+    {
+        $this->commands([
+            AppDbBackupCommand::class
+        ]);
+    }
+}
